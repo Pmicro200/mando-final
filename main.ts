fcc347fc963ebe7a153f6f129054cc3f102f67e7
Pmicro200 <98743108+Pmicro200@users.noreply.github.com>
2022-03-30 09:12:41 +0200

    ala yasta

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
 input.onButtonPressed(Button.A, function () {
     maqueen_andando = 1
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     maqueen_andando = 0
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
 })
 let maqueen_andando = 0
 radio.setGroup(88)
